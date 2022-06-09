1667a890d3074309864335fa0ebaa90153a3d579
Tiffany Rose <tiffany.drose@gmail.com>
2022-06-09 13:06:22 -0400

    scaffolding javascript functions,variables, and event listeners

--- script.js
+++ script.js
@@ -1,10 +1,50 @@
-const arrayOfTasks = [
-  {
-    task: "Go to the vet",
-    checked: false,
-  },
-  {
-    task: "Buy bananas",
-    checked: true,
-  },
-];
+// const arrayOfTasks = [
+//   {
+//     task: "Go to the vet",
+//     checked: false,
+//   },
+//   {
+//     task: "Buy bananas",
+//     checked: true,
+//   },
+// ];
+
+// JS Variables
+// empty arrayOfTasks
+
+
+// DOM Variables
+
+// taskList col div
+
+// individualTask form div
+
+// addTask button
+
+// deleteTask button
+
+
+
+
+// Function Definitions 
+
+// 
+
+// createNewTask() 
+  // generate div
+  // generate object
+  // create 
+
+// deleteTask()
+
+
+
+
+// Event Listeners
+
+// Window.addEventListener("load", create empty task array)
+
+// addTask.addEventListener("submit", create object/with appropriate props)
+  // e.preventDefault()
+
+// deleteTask.addEventListener("click", remove task from array)
