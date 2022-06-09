f981e5af463f9e07ee0f8c988b6c0879116c7605
Tiffany Rose <tiffany.drose@gmail.com>
2022-06-09 19:59:46 -0400

    working on javascript variables

--- script.js
+++ script.js
@@ -7,19 +7,48 @@ const arrayOfTasks = [
     task: "Buy bananas",
     checked: true,
   },
+  {
+    task: "Buy bananas",
+    checked: true,
+  },
+  {
+    task: "Buy bananas",
+    checked: true,
+  },
 ];
+
+for (let i = 0; i < arrayOfTasks.length; i++) {
+  console.log(arrayOfTasks[i].task);
+}
+
+
 
 // js variables
 const newTasks = document.getElementById("task-input");
 
 // Create an array of objects
-for (let i = 0; i < arrayOfTasks.lenth; i++) {
-  console.log(arrayOfTasks[i].task);
 
-}
 // DOM variables
 const newTaskEl = document.createElement("new-task");
-const taskListDiv = document.querySelector(".col-lg.task-list")
+const taskListDiv = document.querySelector("#listItems");
+
+const phoneRowDiv = document.getElementById("phone-row");
+
+for (let i = 0; i < users.length; i++) {
+  console.log(users[i].phone);
+  const newDiv = document.createElement("div");
+  newDiv.className = "col-12";
+
+  const h1El = document.createElement("h1");
+  h1El.innerText = users[i].name;
+  newDiv.append(h1El);
+
+  const pEl = document.createElement("p");
+  pEl.innerText = users[i].phone;
+  newDiv.append(pEl);
+
+  phoneRowDiv.append(newDiv);
+}
 
 newTaskEl.innerHTML = "This is a new task";
 newTaskEl.append("div");
@@ -27,5 +56,18 @@ newTaskEl.append("div");
 // Function Definitions
 
 function createTask() {
+  taskListDiv.innerHTML = "";
 
+  const newDiv = document.createElement("div");
+  const newInput = document.createElement("input");
+  const newLabel = document.createElement("label");
+  const newBtn = document.createElement("button");
+
+  newDiv.className = "form-check";
+  newInput.className = "form-check-input";
+
+  taskListDiv.append(newDiv);
+  newDiv.append(newInput);
 }
+
+// createTask();
