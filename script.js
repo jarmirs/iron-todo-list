be19708f5d5d51a992a196f5cc0780002a9eb387
Julian Sarza <julian2570@gmail.com>
2022-06-09 16:33:17 -0700

    Added some dom manipulation

--- script.js
+++ script.js
@@ -8,3 +8,24 @@ const arrayOfTasks = [
     checked: true,
   },
 ];
+
+// js variables
+const newTasks = document.getElementById("task-input");
+
+// Create an array of objects
+for (let i = 0; i < arrayOfTasks.lenth; i++) {
+  console.log(arrayOfTasks[i].task);
+
+}
+// DOM variables
+const newTaskEl = document.createElement("new-task");
+const taskListDiv = document.querySelector(".col-lg.task-list")
+
+newTaskEl.innerHTML = "This is a new task";
+newTaskEl.append("div");
+
+// Function Definitions
+
+function createTask() {
+
+}
